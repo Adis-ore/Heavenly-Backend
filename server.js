369af7b5a5f17ff125793ad8fb85f24e5706466e
Adis-ore <adisoreoluwa@gmail.com>
2026-04-02 10:20:29 +0100

    cors

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ const comfortRouter = require('./routes/comfort');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS: origin ${origin} not allowed`));
+      }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
@@ -15,7 +33,7 @@ app.get('/api/health', (req, res) => {
 
 app.use('/api', comfortRouter);
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong. Please try again.' });
 });
